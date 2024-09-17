c43a6691c20742c5a599e232f0f620a2738b585d
Belloabraham121 <belloabraham121@gmail.com>
2024-09-17 17:19:01 +0100

    LudoMap

--- contracts/LudoMap.sol
+++ contracts/LudoMap.sol
@@ -1,7 +1,43 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.23;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleLudo {
+  
+    uint8  board_size = 52;
+    uint8  num_players = 4;
+
+    struct Player {
+        address addr;
+        uint8 position;
+    }
+
+    Player[num_players] public players;
+    uint8 public currentPlayerTurn;
 
 
-contract LudoMap {
-   
+    constructor(address player1, address player2, address player3, address player4) {
+        players[0] = Player(player1, 0);
+        players[1] = Player(player2, 0);
+        players[2] = Player(player3, 0);
+        players[3] = Player(player4, 0);
+        currentPlayerTurn = 0;
+    }
+
+    function rollandMove() public {
+        require(msg.sender == players[currentPlayerTurn].addr, "Oga no be your turn");
+    
+        uint8 roll = uint8((uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % 6) + 1);
+        
+      
+        players[currentPlayerTurn].position = (players[currentPlayerTurn].position + roll) % board_size;
+    
+        
+       
+        currentPlayerTurn = currentPlayerTurn == 0  1 : 0;
+    }
+
+    function getPlayerPosition(uint8 playerIndex) public view returns (uint8) {
+        require(playerIndex < num_players, "Player index no dey");
+        return players[playerIndex].position;
+    }
 }
